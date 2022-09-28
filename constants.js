7174bd1eec19d87653295d064b8263a895b1c9a3
Nicholas Kovacs <nicholasderekkovacs@gmail.com>
2022-09-28 18:27:39 +0000

    constants module

---
+++ constants.js
@@ -0,0 +1,15 @@
+let IP = "165.227.47.243"
+let PORT = "50541"
+let MOVE_UP_KEY = "w"
+let MOVE_DOWN_KEY= "s"
+let MOVE_LEFT_KEY = "a"
+let MOVE_RIGHT_KEY = "d"
+
+module.exports = {
+  IP,
+  PORT,
+  MOVE_DOWN_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_RIGHT_KEY,
+  MOVE_UP_KEY
+};
--- input.js
+++ input.js
@@ -1,5 +1,5 @@
 let connection;
-
+const { MOVE_DOWN_KEY, MOVE_LEFT_KEY, MOVE_RIGHT_KEY, MOVE_UP_KEY} = require("./constants");
 
 const setupInput = conn => {
   connection = conn;
@@ -16,20 +16,20 @@ const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
   }
-  if (key === 'w') {
+  if (key === MOVE_UP_KEY) {
     connection.write("Move: up");
   }
-  if (key === 'd') {
+  if (key === MOVE_RIGHT_KEY) {
     connection.write("Move: right");
   }
-  if (key === 'a') {
+  if (key === MOVE_LEFT_KEY) {
     connection.write("Move: left");
   }
-  if (key === 's') {
+  if (key === MOVE_DOWN_KEY) {
     connection.write("Move: down");
   }
   if (key === 'g') {
-    connection.write("Say: gg");
+    connection.write("Say: Well Played");
   }
   if (key === 'n') {
     connection.write("Say: Nice Save!");
